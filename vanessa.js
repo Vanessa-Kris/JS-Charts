60dd8bb9a6102345e8bdfed7204c28421a461aed
Minion <nessakristo@gmail.com>
2022-07-29 23:12:36 +0100

    some rubbish

--- vanessa.js
+++ vanessa.js
@@ -1,90 +1,467 @@
 // Load the Visualization API and the corechart package.
-google.charts.load('current', {
-    'packages': ['corechart']
-});
+// google.charts.load('current', {
+//     'packages': ['corechart']
+// });
 
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.setOnLoadCallback(drawChart);
 
-    // Callback that creates and populates a data table,
-    // instantiates the chart, passes in the data and
-    // draws it.
+// Callback that creates and populates a data table,
+// instantiates the chart, passes in the data and
+// draws it.
+// function drawChart() {
+
+
+// My data arr
+const entry = [{
+        Month: 'Days',
+        Orange: 'Orange subscribers',
+        Camtel: 'Camtel Subscribers',
+        MTN: 'MTN Subscribers',
+        Nextel: 'Nextel'
+    },
+    {
+        Month: 'Day1',
+        Orange: 20,
+        Camtel: 31,
+        MTN: 0,
+        Nextel: 10
+    },
+    {
+        Month: 'Day2',
+        Orange: 20,
+        Camtel: 29,
+        MTN: 6,
+        Nextel: 30
+    },
+    {
+        Month: 'Day3',
+        Orange: 10,
+        Camtel: 31,
+        MTN: 8,
+        Nextel: 20
+    },
+    {
+        Month: 'Day4',
+        Orange: 20,
+        Camtel: 30,
+        MTN: 9,
+        Nextel: 31
+    },
+    {
+        Month: 'Day5',
+        Orange: 22,
+        Camtel: 31,
+        MTN: 12,
+        Nextel: 12
+    },
+    {
+        Month: 'Day6',
+        Orange: 20,
+        Camtel: 30,
+        MTN: 20,
+        Nextel: 14
+    },
+    {
+        Month: 'Day7',
+        Orange: 20,
+        Camtel: 31,
+        MTN: 25,
+        Nextel: 16
+    },
+    {
+        Month: 'Day8',
+        Orange: 20,
+        Camtel: 31,
+        MTN: 0,
+        Nextel: 20
+    },
+    {
+        Month: 'Day9',
+        Orange: 30,
+        Camtel: 30,
+        MTN: 20,
+        Nextel: 30
+    },
+    {
+        Month: 'Day10',
+        Orange: 20,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 31
+    },
+    {
+        Month: 'Day11',
+        Orange: 20,
+        Camtel: 30,
+        MTN: 20,
+        Nextel: 10
+    },
+    {
+        Month: 'Day12',
+        Orange: 40,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 6
+    },
+    {
+        Month: 'Day13',
+        Orange: 30,
+        Camtel: 21,
+        MTN: 20,
+        Nextel: 3
+    },
+    {
+        Month: 'Day14',
+        Orange: 40,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 10
+    },
+    {
+        Month: 'Day15',
+        Orange: 20,
+        Camtel: 21,
+        MTN: 20,
+        Nextel: 3
+    },
+    {
+        Month: 'Day16',
+        Orange: 10,
+        Camtel: 11,
+        MTN: 20,
+        Nextel: 22
+    },
+    {
+        Month: 'Day17',
+        Orange: 40,
+        Camtel: 30,
+        MTN: 20,
+        Nextel: 30
+    },
+    {
+        Month: 'Day18',
+        Orange: 30,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 30
+    },
+    {
+        Month: 'Day19',
+        Orange: 20,
+        Camtel: 3,
+        MTN: 20,
+        Nextel: 20
+    },
+    {
+        Month: 'Day20',
+        Orange: 10,
+        Camtel: 1,
+        MTN: 20,
+        Nextel: 10
+    },
+    {
+        Month: 'Day21',
+        Orange: 40,
+        Camtel: 1,
+        MTN: 20,
+        Nextel: 30
+    },
+    {
+        Month: 'Day22',
+        Orange: 10,
+        Camtel: 13,
+        MTN: 20,
+        Nextel: 13
+    },
+    {
+        Month: 'Day23',
+        Orange: 20,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 31
+    },
+    {
+        Month: 'Day24',
+        Orange: 40,
+        Camtel: 4,
+        MTN: 20,
+        Nextel: 30
+    },
+    {
+        Month: 'Day25',
+        Orange: 30,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 12
+    },
+    {
+        Month: 'Day26',
+        Orange: 21,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 10
+    },
+    {
+        Month: 'Day27',
+        Orange: 4,
+        Camtel: 8,
+        MTN: 20,
+        Nextel: 0
+    },
+    {
+        Month: 'Day28',
+        Orange: 40,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 0
+    },
+    {
+        Month: 'Day29',
+        Orange: 40,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 10
+    },
+    {
+        Month: 'Day30',
+        Orange: 40,
+        Camtel: 31,
+        MTN: 20,
+        Nextel: 10
+    }
+];
+
+// Create the data table.
+// Call data using map
+// var data = google.visualization.arrayToDataTable(
+//     entry.map(function (element) {
+//         result = []
+//         result.push(element.Month)
+//         result.push(element.Orange)
+//         // result.push(element.Camtel)
+//         // result.push(element.MTN)
+//         // result.push(element.Nextel)
+//         return result;
+//     })
+
+// );
+
+
+// Set chart options
+// var options = {
+//     vAxis: {
+//         title: 'Days',
+//         format: '0',
+
+//     },
+//     hAxis: {
+//         title: 'January'
+//     },
+//     // 'width': 1000,
+//     // 'height': 800
+//     'title': 'Daily Subscribers Visualization',
+
+// };
+
+// Instantiate and draw our chart, passing in some options.
+//     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+//     chart.draw(data, options);
+// }
+// document.getSelection("operator").addEventListener("click", function () {
+//     let operator = document.getElementById("operator").value;
+
+
+//MTN!
+function mymtnFunction() {
+    // if("operator" == "mtn") {
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
     function drawChart() {
+        var data = google.visualization.arrayToDataTable(
+            entry.map(function (element) {
+                result = []
+                result.push(element.Month)
+                result.push(element.MTN)
+                // result.push(element.Camtel)
+                // result.push(element.MTN)
+                // result.push(element.Nextel)
+                return result;
+            })
+
+        );
 
 
-        // My data arr
-    const entry = [
-        {Month: 'Days', Orange: 'Orange subscribers', Camtel: 'Camtel Subscribers', MTN: 'MTN Subscribers', Nextel: 'Nextel'},
-        {Month: 'Day1', Orange: 20, Camtel: 31, MTN: 0,  Nextel: 10},
-        {Month: 'Day2', Orange: 20, Camtel: 29, MTN: 6,  Nextel: 30},
-        {Month: 'Day3', Orange: 10, Camtel: 31, MTN: 8,  Nextel: 20},
-        {Month: 'Day4', Orange: 20, Camtel: 30, MTN: 9,  Nextel: 31},
-        {Month: 'Day5', Orange: 22, Camtel: 31, MTN: 12,  Nextel: 12},
-        {Month: 'Day6', Orange: 20, Camtel: 30, MTN: 20,  Nextel: 14},
-        {Month: 'Day7', Orange: 20, Camtel: 31, MTN: 25,  Nextel: 16},
-        {Month: 'Day8', Orange: 20, Camtel: 31, MTN: 0,  Nextel: 20},
-        {Month: 'Day9', Orange: 30, Camtel: 30, MTN: 20,  Nextel: 30},
-        {Month: 'Day10', Orange: 20, Camtel: 31, MTN: 20,  Nextel: 31},
-        {Month: 'Day11', Orange: 20, Camtel: 30, MTN: 20,  Nextel: 10},
-        {Month: 'Day12', Orange: 40, Camtel: 31, MTN: 20,  Nextel: 6},
-        {Month: 'Day13', Orange: 30, Camtel: 21, MTN: 20,  Nextel: 3},
-        {Month: 'Day14', Orange: 40, Camtel: 31, MTN: 20,  Nextel: 10},
-        {Month: 'Day15', Orange: 20, Camtel: 21, MTN: 20,  Nextel: 3},
-        {Month: 'Day16', Orange: 10, Camtel: 11, MTN: 20,  Nextel: 22},
-        {Month: 'Day17', Orange: 40, Camtel: 30, MTN: 20,  Nextel: 30},
-        {Month: 'Day18', Orange: 30, Camtel: 31, MTN: 20,  Nextel: 30},
-        {Month: 'Day19', Orange: 20, Camtel: 3, MTN: 20,  Nextel: 20},
-        {Month: 'Day20', Orange: 10, Camtel: 1, MTN: 20,  Nextel: 10},
-        {Month: 'Day21', Orange: 40, Camtel: 1, MTN: 20,  Nextel: 30},
-        {Month: 'Day22', Orange: 10, Camtel: 13, MTN: 20,  Nextel: 13},
-        {Month: 'Day23', Orange: 20, Camtel: 31, MTN: 20,  Nextel: 31},
-        {Month: 'Day24', Orange: 40, Camtel: 4, MTN: 20,  Nextel: 30},
-        {Month: 'Day25', Orange: 30, Camtel: 31, MTN: 20,  Nextel: 12},
-        {Month: 'Day26', Orange: 21, Camtel: 31, MTN: 20,  Nextel: 10},
-        {Month: 'Day27', Orange: 4, Camtel: 8, MTN: 20,  Nextel: 0},
-        {Month: 'Day28', Orange: 40, Camtel: 31, MTN: 20,  Nextel: 0},
-        {Month: 'Day29', Orange: 40, Camtel: 31, MTN: 20,  Nextel: 10},
-        {Month: 'Day30', Orange: 40, Camtel: 31, MTN: 20,  Nextel: 10}
+        // Set chart options
+        var options = {
+            vAxis: {
+                title: 'Days',
+                format: '0',
+
+            },
+            hAxis: {
+                title: 'January'
+            },
+            //     // 'width': 1000,
+            //     // 'height': 800
+            'title': 'Daily Subscribers Visualization',
+
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
+}
+// }
+// )
+
+// Orange!
+function myorangeFunction() {
+    // if("operator" == "mtn") {
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
 
 
-    ];
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
 
-    
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable(
+            entry.map(function (element) {
+                result = []
+                result.push(element.Month)
+                result.push(element.Orange)
+                // result.push(element.Camtel)
+                // result.push(element.MTN)
+                // result.push(element.Nextel)
+                return result;
+            })
 
-    // Create the data table.
-    // Call data using map
-    var data = google.visualization.arrayToDataTable (
-        entry.map(function(element){
-            result = [ ]
-            result.push(element.Month)
-            result.push(element.Orange)
-            result.push(element.Camtel)
-            result.push(element.MTN)
-            result.push(element.Nextel)
-            return result;
-        })
-       
-    );
+        );
 
-    
-    // Set chart options
-    var options = { 
-        vAxis: {
-            title: 'Days',
-            format: '0',
-           
-        },
-        hAxis: {
-            title: 'January'
-        },
-        // 'width': 1000,
-        // 'height': 800
-        'title':'Daily Subscribers Visualization',
 
-    };
+        // Set chart options
+        var options = {
+            vAxis: {
+                title: 'Days',
+                format: '0',
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
+            },
+            hAxis: {
+                title: 'January'
+            },
+            //     // 'width': 1000,
+            //     // 'height': 800
+            'title': 'Daily Subscribers Visualization',
+
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
 }
 
+
+// Camtel!
+function mycamtelFunction() {
+    // if("operator" == "mtn") {
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable(
+            entry.map(function (element) {
+                result = []
+                result.push(element.Month)
+                // result.push(element.MTN)
+                result.push(element.Camtel)
+                // result.push(element.MTN)
+                // result.push(element.Nextel)
+                return result;
+            })
+
+        );
+
+
+        // Set chart options
+        var options = {
+            vAxis: {
+                title: 'Days',
+                format: '0',
+
+            },
+            hAxis: {
+                title: 'January'
+            },
+            //     // 'width': 1000,
+            //     // 'height': 800
+            'title': 'Daily Subscribers Visualization',
+
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
+}
+
+// Nextel!
+function mynextelFunction() {
+    // if("operator" == "mtn") {
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+     
+        
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawChart);
+    
+        function drawChart() {
+        var data = google.visualization.arrayToDataTable(
+            entry.map(function (element) {
+                result = []
+                result.push(element.Month)
+                // result.push(element.MTN)
+                // result.push(element.Camtel)
+                // result.push(element.MTN)
+                result.push(element.Nextel)
+                return result;
+            })
+    
+        );
+    
+    
+        // Set chart options
+        var options = {
+            vAxis: {
+                title: 'Days',
+                format: '0',
+    
+            },
+            hAxis: {
+                title: 'January'
+            },
+        //     // 'width': 1000,
+        //     // 'height': 800
+            'title': 'Daily Subscribers Visualization',
+    
+        };
+    
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+        }}
